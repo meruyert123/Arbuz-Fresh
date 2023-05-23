dc61352164d91cddc4dc067c24c340fefe0a89d9
meruyert123 <meruertzhunusova1711@gmail.com>
2023-05-23 19:16:23 +0600

    Add correct options time interval in OptionsToConfirm struct

--- Arbuz Fresh/Supports/Constants/OptionsToConfirm.swift
+++ Arbuz Fresh/Supports/Constants/OptionsToConfirm.swift
@@ -2,7 +2,7 @@ import Foundation
 
 struct OptionsToConfirm {
     static let optionsDayOfWeek = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
-    static let optionsTimeInterval = ["9:00-12:00", "9:00-12:00", "9:00-12:00", "9:00-12:00", "9:00-12:00", "9:00-12:00", "9:00-12:00"]
+    static let optionsTimeInterval = ["9:00-12:00", "12:00-15:00", "15:00-18:00", "18:00-21:00", "21:00-00:00"]
     static let optionsSubscriptions = ["1 неделю", "2 недели", "3 недели", "4 недели", "1 месяц", "2 месяца", "3 месяца", "полгода"]
     
 }
