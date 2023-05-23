dc170db3bea48973ae00c56a1eef1aa6ca25dd77
meruyert123 <meruertzhunusova1711@gmail.com>
2023-05-23 19:38:42 +0600

    Correct regex

--- Arbuz Fresh/Supports/Regex/Patterns/Regex.swift
+++ Arbuz Fresh/Supports/Regex/Patterns/Regex.swift
@@ -1,11 +1,8 @@
 import Foundation
 
 enum RegexPatterns {
-    static let emailChars = ".*[@].*"
-    static let higherThanSixChars = "^.{6,}$"
-    static let name = "^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$"
     static let phonenumber = "^\\+?\\d{11}$"
     static let address = "[А-Яа-я0-9,.-]{1,}"
     static let digitRegex = "^[0-9]+$"
-    static let alphanumericRegex = "^[a-zA-Z0-9]+$"
+    static let alphanumericRegex = "[А-Яа-я]"
 }
